291c88cbcff327a244551b8ed7c78537512dd4b2
gunnikaa <gunnu944@gmail.com>
2026-03-10 11:52:41 +0530

    AppID

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index ():
-    return "<h1>Gunn Mulchandani</h1>"
+def index():
+    return "<h1>Gunn Mulchandani</h1><br><h1>App ID = 2410731</h1>"
 
 if __name__ == "__main__":
-    app.run(host= "0.0.0.0", port=5000, debug=True)
-    
+    app.run(host="0.0.0.0", port=5000, debug=True)
